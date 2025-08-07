eebc5a8ae173d2f54d6af2b64416cb02101a60db
Leo Dulanto <Leotens11@gmail.com>
2025-08-07 16:43:43 +0000

    Sigue estando en login al inicio

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
-import { useRouter } from 'next/navigation';
 
 type User = {
   username: string;
